eb033f4dc7196f4307eba1a70b4f4ee1c0e0f945
shreyshreygowda <156389514+shreyshreygowda@users.noreply.github.com>
2025-04-25 23:58:38 -0700

    Update theme.ts

---
+++ components/theme.ts
@@ -0,0 +1,9 @@
+// constants/theme.ts
+export const colors = {
+    background: '#0E0A1F',
+    primaryText: '#F0EFFF',
+    secondaryText: '#B0AFC2',
+    accent: '#9A88F2',
+    accentSoft: '#C0A4FD',
+    gradient: ['#8566CC', '#F9CFF4'],
+  };
